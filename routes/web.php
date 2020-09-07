88eec687bcfda735133cf99b4d7498aafcc2a058
hossam <hossamameen948@gmail.com>
2020-09-07 11:43:27 +0200

    add dashboard

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+   
+   return redirect()->route('login');
+   
 });
 Route::prefix('admin')->namespace('DashBoard')->group(function(){
     Route::middleware('auth')->group(function () {

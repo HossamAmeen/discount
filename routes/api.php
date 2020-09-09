615a125bfbd2270172b25b55f6b7f6a6e1a4107e
hossam <hossamameen948@gmail.com>
2020-09-10 01:07:08 +0200

    add city

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
 
 // Route::prefix('admin')->namespace('DashBoard')->group(function(){
 

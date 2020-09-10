a047d9949f7ce44b432822b31ff8e1c8d6eb0cb0
hossam <hossamameen948@gmail.com>
2020-09-10 13:19:24 +0200

    edit validation

--- app/Http/Requests/Client/ClientRequest.php
+++ app/Http/Requests/Client/ClientRequest.php
@@ -33,8 +33,8 @@ class ClientRequest extends FormRequest
             'password' => ['string'],
             'phone' => ['numeric', 'digits_between:11,11' ,Rule::unique('clients')->ignore($id)->whereNull('deleted_at')],
             'image' => ['image'],
-            'facebook_id' => ['numeric' , Rule::unique('clients')->ignore($id)->whereNull('deleted_at')],
-            "google_id" => ['numeric' , Rule::unique('clients')->ignore($id)->whereNull('deleted_at')],
+            'facebook_id' => ['numeric' , Rule::unique('clients')->ignore($id)->whereNull('deleted_at') ,'nullable'],
+            "google_id" => ['numeric' , Rule::unique('clients')->ignore($id)->whereNull('deleted_at'),'nullable'],
         ];
         // if ($this->isMethod('POST') )
         if(strpos($this->fullUrl(), "register") !== false)

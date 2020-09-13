deacbeec43bb2000ef94beb8a48b90ccff460165
hossam <hossamameen948@gmail.com>
2020-09-14 00:41:44 +0200

    repair cities:

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,11 +1,13 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Http\Controllers\APIResponseTrait;
 use App\Models\{City,Category};
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
+    use APIResponseTrait;
     /**
      * Create a new controller instance.
      *

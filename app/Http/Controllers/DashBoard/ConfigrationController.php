a74277d16ac4a5522049364cf0f83bf31797e359
hossam <hossamameen948@gmail.com>
2020-10-05 12:41:07 +0200

    show top product

--- app/Http/Controllers/DashBoard/ConfigrationController.php
+++ app/Http/Controllers/DashBoard/ConfigrationController.php
@@ -38,8 +38,8 @@ class ConfigrationController extends BackEndController
         $configrationSite['today_orders'] =  $orders->wheredate('created_at' , date('Y-m-d'))->count();
         $configrationSite['products'] = $products->count();
         // return $top_product;
-        $configrationSite['top_product'] =$top_product->name. '('.$top_product->vendor->store_name ?? " ".')';
-
+        $configrationSite['top_product'] =$top_product->name. '('. ( $top_product->vendor->store_name ??  " " ).')';
+       
         
 
             // $top_product = \App\Models\Order::pluck('product_id')->toArray();
